4f34ef02bb9cbd99d1d012b7e39e1982add485bb
mirosh_kavinda <kavindahgm@gmail.com>
2022-11-28 02:01:03 +0530

    ss

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/index.css";
 import App from "./App";
-import { HashRouter } from 'react-router-dom'
+
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
